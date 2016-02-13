32008723282927547636967f33feb8420cdda3a4
Emile Ndagijimana <emilendagijimana2@gmail.com>
2016-02-13 14:06:18 +0100

    Added Gestion_Match_FootBall project

---
+++ Gestion_Match_FootBall/Gestion_Match_FootBall/Model/But.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Match_FootBall.Model
+{
+    public class But
+    {
+        public int ID_BUT { get; set; }
+        public int ID_JOUEUR { get; set; }
+    }
+}

e83bd74522f4e8ceeb1b594a1eeca1c22957fd74
Emile Ndagijimana <emilendagijimana2@gmail.com>
2016-02-20 19:25:00 +0100

    ok

--- Gestion_Match_FootBall/Gestion_Match_FootBall/ViewModel/EquipeViewModel.cs
+++ Gestion_Match_FootBall/Gestion_Match_FootBall/ViewModel/EquipeViewModel.cs
@@ -1,15 +1,33 @@
-﻿using Gestion_Match_FootBall.Model;
-using System;
+﻿using System;
+using System.Collections.ObjectModel;
+using Gestion_Match_FootBall.Data_Acces;
+using Gestion_Match_FootBall.Model;
 
 namespace Gestion_Match_FootBall.ViewModel
 {
     public class EquipeViewModel : ViewModel_Base
     {
+        readonly EquipesRepository _equipesRepository;
+        public ObservableCollection<Equipe> Tous_les_equipes
+        {
+            get;
+            private set;
+        }
         private Equipe _equipe;
         
         public EquipeViewModel()
         {
             _equipe = new Equipe();
+        }
+        public EquipeViewModel(EquipesRepository equipeRepository)
+        {
+            if (equipeRepository == null)
+                throw new ArgumentNullException("equipeRepository");
+            _equipesRepository = equipeRepository;
+
+            // My list of team in view model
+
+            Tous_les_equipes = new ObservableCollection<Equipe>(_equipesRepository.GetEquipes());
         }
         // Let's Create properties that will bind with my UI (of creating a team)
 
--- Gestion_Match_FootBall/Gestion_Match_FootBall/ViewModel/MainWindow_ViewMode.cs
+++ Gestion_Match_FootBall/Gestion_Match_FootBall/ViewModel/MainWindow_ViewMode.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.ObjectModel;
+using Gestion_Match_FootBall.Data_Acces;
+
 namespace Gestion_Match_FootBall.ViewModel
 {
     public class MainWindow_ViewMode : ViewModel_Base
     {
+        readonly EquipesRepository _equipesRepository;
         private ObservableCollection<ViewModel_Base> _view_Models;
         public MainWindow_ViewMode()
         {
-            // create an instance of my view model and add it to my collection 
-            EquipeViewModel equipe_viewModel = new EquipeViewModel();
+            _equipesRepository = new EquipesRepository();
+             // create an instance of my view model and add it to my collection 
+             EquipeViewModel equipe_viewModel = new EquipeViewModel();
             this.ViewModels.Add(equipe_viewModel);
         } 
 
